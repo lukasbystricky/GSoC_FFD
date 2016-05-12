1c2c098b30ef889a2542f4bee8c56b41c60d6a5b
Lukas <Lukas>
2016-05-12 13:54:58 -0400

    Initial commit of fast fluid solver. Added very basic C# outline of code.

---
+++ FluidSolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFluidSolver
+{
+    class FluidSolver
+    {
+        double[] u;
+        double[] v;
+        double[] w;
+        double[] p;
+
+        void add_force();
+        void diffuse();
+        void project();
+        void advect();
+    }
+}
