5b478676867f229e67be12573c3af19f523cbfd0
Lukas <Lukas>
2016-05-15 10:48:23 -0400

    Added domain interface, support for boundary condition in solver method.

---
+++ Domain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFluidSolver
+{
+    interface Domain
+    {
+        int N;
+        double h;
+
+        int[] bounary_nodes;   //flag to indicate if node is on a boundary
+        int[] obstacle;        //flag to indicate if node is part of an obstacle
+        int[] boundary_normal_x; //flag to indicate if boundary at node is normal to x direction
+        int[] boundary_normal_y; //flag to indicate if boundary at node is normal to y direction
+        int[] boundary_normal_z; //flag to indicate if boundary at node is normal to z direction
+        double[] boundary_u;   //x component of velocity at boundary
+        double[] boundary_v;   //y component of velocity at boundary
+        double[] boundary_w;   //z component of velocity at boundary
+    }
+}
--- FluidSolver.cs
+++ FluidSolver.cs
@@ -17,10 +17,16 @@ namespace FastFluidSolver
         private double[] w; // z component of velocity
         private double[] p; // pressure
 
+        private double[] u_sratch; //scratch arrays for velocities
+        private double[] v_sratch;
+        private double[] w_sratch;
+
         private double dt;  //time step
         private int N;      //number of points in each coordiate direction
         private double h;   //spacing in each corrdiate direction
         private double nu;  //fluid viscosity
+
+        Domain omega;
 
         void initialize();
         void add_force();
@@ -32,7 +38,8 @@ namespace FastFluidSolver
         void diffuse(ref double[] x)
         {
             double[] x_old = x;
-            gs_solve(1 + 6 * nu * dt / Math.Pow(h, 2), dt * nu / Math.Pow(h, 2), x_old, ref x);
+            apply_boundary_conditions();
+            gs_solve(1 + 6 * nu * dt / Math.Pow(h, 2), dt * nu / Math.Pow(h, 2), x_old, ref x, 0);
         }
 
         /*****************************************************************************
@@ -59,7 +66,7 @@ namespace FastFluidSolver
                 }
             }
 
-            gs_solve(6 / Math.Pow(h, 2), 1 / Math.Pow(h, 2), div_w, ref p);
+            gs_solve(6 / Math.Pow(h, 2), 1 / Math.Pow(h, 2), div_w, ref p, 1);
 
             //update velocity by adding calculate grad(p), calculated using second order finite difference
             for (int i = 0; i < N; i++)
@@ -77,8 +84,23 @@ namespace FastFluidSolver
 
         }
 
-        void advect();
-        void export_vtk();
+        /***************************************************************************
+         * Advection step. Uses a first order backtrace to update x.
+         * 
+         * @inputs
+         * double[] x - reference to quantity to advect, can be a velocity, a
+         * concentration or temperature
+         * double[] x0 - initial state of x before advection
+         * double[] velx - x velocity (either before convection if x is a velocity.
+         * or after convection otherwise)
+         * double[] vely - y velocity (either before convection if x is a velocity.
+         * or after convection otherwise)
+         * double[] velz - z velocity (either before convection if x is a velocity.
+         * or after convection otherwise)
+         **************************************************************************/
+        void advect(ref double[] x, double[] x0, double[] velx, double[] vely,  double[] velz)
+        {
+        }
 
         /*****************************************************************************
          * Solves the banded system given by the finite difference method applied
@@ -88,36 +110,81 @@ namespace FastFluidSolver
          * double c - coefficient of all other nonzero entries
          * double[] b - right hand side
          * double[] x - reference to array in which to store solution
+         * int boundary type - 0 corresponds to Dirichlet, 1 to homogeneous Neumann 
          * 
          * TO DO: add boundary conditions
          ****************************************************************************/
-        void gs_solve(double a, double c, double[] b, ref double[] x)
+        void gs_solve(double a, double c, double[] b, ref double[] x, int boundary_type)
         {
 
             int iter = 0;
-            double err = 2*TOL;
-            while (iter < MAX_ITER && err > TOL)
+            double res = 2*TOL;
+            while (iter < MAX_ITER && res > TOL)
             {
-                err = 0;
+                res = 0;
+
                 for (int i = 0; i < N; i++)
                 {
                     for (int j = 0; j < N; j++)
                     {
                         for (int k = 0; k < N; k++)
                         {
-                            double x_old = x[cell_index(i, j, k)];
-                            x[cell_index(i, j, k)] = (b[cell_index(i, j, k)] - c * (x[cell_index(i - 1, j, k)] +
-                                x[cell_index(i + 1, j, k)] + x[cell_index(i, j - 1, k)] + x[cell_index(i, j + 1, k)] +
-                                x[cell_index(i, j, k - 1)] + x[cell_index(i, j, k + 1)])) / a;
+                            if (omega.obstacle[cell_index(i, j, k)] == 0) //if part of fluid domain
+                            {
+                                double x_old = x[cell_index(i, j, k)];
 
-                            err = err + Math.Pow(x_old - x[cell_index(i, j, k)], 2)/Math.Pow(N,3);
+                                if (omega.bounary_nodes[cell_index(i, j, k)] == 0) //if not on boundary
+                                {
+                                        
+                                        x[cell_index(i, j, k)] = (b[cell_index(i, j, k)] - c * (x[cell_index(i - 1, j, k)] +
+                                            x[cell_index(i + 1, j, k)] + x[cell_index(i, j - 1, k)] + x[cell_index(i, j + 1, k)] +
+                                            x[cell_index(i, j, k - 1)] + x[cell_index(i, j, k + 1)])) / a;
+                                }
+                                else if (boundary_type == 1)//if on boundary and homogeneous Neumann boundary conditions
+                                {
+                                        int nx = omega.boundary_normal_x[cell_index(i, j, k)];
+                                        int ny = omega.boundary_normal_x[cell_index(i, j, k)];
+                                        int nz = omega.boundary_normal_x[cell_index(i, j, k)];
+
+                                        x[cell_index(i, j, k)] = (b[cell_index(i, j, k)] - c * (Math.Abs(1 + nx) * x[cell_index(i - 1, j, k)] +
+                                            Math.Abs(1 - nx) * x[cell_index(i + 1, j, k)] + Math.Abs(1 + ny) * x[cell_index(i, j - 1, k)] +
+                                            Math.Abs(1 - ny) * x[cell_index(i, j + 1, k)] + Math.Abs(1 + nz) * x[cell_index(i, j, k - 1)] +
+                                            Math.Abs(1 - nz) * x[cell_index(i, j, k + 1)])) / a;
+
+                                       
+                                } 
+                                    
+                                    res = res + Math.Pow(x_old - x[cell_index(i, j, k)], 2) / Math.Pow(N, 3);
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
 
-                err = Math.Sqrt(err);
+                res = Math.Sqrt(res);
             }
-            
+
+        /*********************************************************************************
+         * Applies the boundary conditions from omega to the velocities
+         ********************************************************************************/
+        void apply_boundary_conditions()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        if (omega.bounary_nodes[cell_index(i, j, k)] == 1)
+                        {
+                            u[cell_index(i, j, k)] = omega.boundary_u[cell_index(i, j, k)];
+                            v[cell_index(i, j, k)] = omega.boundary_v[cell_index(i, j, k)];
+                            w[cell_index(i, j, k)] = omega.boundary_w[cell_index(i, j, k)];
+                        }
+                    }
+                }
+            }
         }
 
         /***************************************************************************
@@ -125,8 +192,11 @@ namespace FastFluidSolver
          **************************************************************************/
         int cell_index(int x, int y, int z)
         {
-            return x + y * N + z * N ^ 2;
+            return (x >= 0 && y >=0 && z >= 0) ? x + y * N + z * N ^ 2 : 0;
         }
+
+
+        void export_vtk();
 
     }
 }
