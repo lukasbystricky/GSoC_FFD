4d002f16c863c56fb4628d4cb64884b4f975ace3
Lukas <Lukas>
2016-08-16 16:02:46 -0400

    Added comments, cleaned up code

--- Utilities.cs
+++ Utilities.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace FastFluidSolver
 {    
+    /// <summary>
+    /// Ultilities that are common to multiple classes.
+    /// </summary>
     public static class Utilities
     {
-        const double EPS = 1e-8;
+        const double EPS = 1e-12;
 
-        /*****************************************************************************
-         * Performs a trilinear interpolation 
-         * 
-         * Method based on code in "Fluid Flow for the Rest of Us: Tutorial of the
-         * Marker and Cell Method in Computer Graphics" by Cline, Cardon and Egbert
-        ****************************************************************************/
+        /// <summary>
+        /// Performs a trilinear interpolation. Method based on code in "Fluid Flow for 
+        /// the Rest of Us: Tutorial of the Marker and Cell Method in Computer Graphics" 
+        /// by Cline, Cardon and Egbert.
+        /// </summary>
+        /// <param name="x">Cell number in x direction</param>
+        /// <param name="y">Cell number in y direction</param>
+        /// <param name="z">Cell number in z direction</param>
+        /// <param name="array">Array to interpolate</param>
+        /// <returns>Interpolated value</returns>
+        /// <remarks>Cell number here is the cell number on the underlying grid of the array. 
+        /// They can be fractional and do not include ghost cells.</remarks>
         public static double trilinear_interpolation(double x, double y, double z, 
                     double[, ,] array)
         {
@@ -37,9 +46,12 @@ namespace FastFluidSolver
                     (x - imin) * (y - jmin) * (z - kmin) * array[imax, jmax, kmax];
         }
 
-        /**************************************************************************
-         * Computes the normalized L2 difference between two 3 dimensional arrays
-         **************************************************************************/
+        /// <summary>
+        /// Computes the pointwise L2 difference between 2 multidimensional arrays.
+        /// </summary>
+        /// <param name="x1">Array 1</param>
+        /// <param name="x2">Array 2</param>
+        /// <returns>Normalized pointwise L2 difference between array 1 and array 2.</returns>
         public static double compute_L2_difference(double[, ,] x1, double[, ,] x2)
         {
             double diff = 0;
@@ -63,10 +75,14 @@ namespace FastFluidSolver
             return diff;
         }
 
-        /***********************************************************************
-         * Checks if a point is inside the domain omega. Returns true if point
-         * is inside domain or on a boundary, false otherwise
-         ***********************************************************************/
+
+        /// <summary>
+        /// Checks if a point is inside the domain.
+        /// </summary>
+        /// <param name="coordinate">coordiate (x,y,z) of point</param>
+        /// <param name="omega">Domain</param>
+        /// <returns>True if point is inside fluid domain or on boundary, false if point is
+        /// outside domain or inside and obstacle/ghost cell.</returns>
         public static bool in_domain(double[] coordinate, Domain omega)
         {
             double hx = omega.hx;
@@ -79,8 +95,8 @@ namespace FastFluidSolver
             int kdomain_min = Math.Max(Math.Min((int)Math.Floor((1 - EPS) * (coordinate[2] + hz) / hz), omega.Nz - 1), 0);
 
             int idomain_max = Math.Max(Math.Min((int)Math.Floor((1 + EPS) * (coordinate[0] + hx) / hx), omega.Nx - 1), 0);
-            int jdomain_max = Math.Max(Math.Min((int)Math.Floor((1 + EPS) * (coordinate[1] + hy) / hy), omega.Nx - 1), 0);
-            int kdomain_max = Math.Max(Math.Min((int)Math.Floor((1 + EPS) * (coordinate[2] + hz) / hz), omega.Nx - 1), 0);
+            int jdomain_max = Math.Max(Math.Min((int)Math.Floor((1 + EPS) * (coordinate[1] + hy) / hy), omega.Ny - 1), 0);
+            int kdomain_max = Math.Max(Math.Min((int)Math.Floor((1 + EPS) * (coordinate[2] + hz) / hz), omega.Nz - 1), 0);
 
 
             List<int> possibleCelli = new List<int>();
@@ -118,12 +134,90 @@ namespace FastFluidSolver
                             indomain = true;
                             break;
                         }
-
                     }
                 }
             }
 
             return indomain;
         }
+
+        /// <summary>
+        /// Computes L2 and L-infinity error between FFD approximation and exact solution.
+        /// </summary>
+        /// <param name="fs">FluidSolver containing FFD solution at time t</param>
+        /// <pram name="omega">Domain on which exact solution is given</pram>
+        /// <param name="err_l2">Normalized pointwise L2 error</param>
+        /// <param name="err_inf">Pointwise L-infinity error</param>
+        /// <param name="t">Time</param>
+        /// <param name="component">Component to evaluate</param>
+        /// <remarks>The component to evaluate must be one of:
+        ///     1 for pressure
+        ///     2 for x component of velocity
+        ///     3 for y component of velocity
+        ///     4 for z component of velocity</remarks>
+        public static void calculate_errors(FluidSolver fs, Domain omega, out double err_l2, out double err_inf,
+            double t, int component)
+        {
+            DataExtractor de = new DataExtractor(omega, fs);
+
+            double nu = fs.nu;
+
+            int Nx = omega.Nx - 1;
+            int Ny = omega.Ny - 1;
+            int Nz = omega.Nz - 1;
+
+            double[, ,] err_array = new double[Nx, Ny, Nz];
+            double[, ,] comp_interp = new double[Nx, Ny, Nz];
+            double[, ,] zeros = new double[Nx, Ny, Nz];
+
+            for (int i = 0; i < Nx; i++)
+            {
+                for (int j = 0; j < Ny; j++)
+                {
+                    for (int k = 0; k < Nz; k++)
+                    {
+                        double x = i * omega.hx;
+                        double y = j * omega.hy;
+                        double z = k * omega.hz;
+
+                        double[] coordinate = new double[] {x,y,z};
+
+                        double[] velocity_interp = de.get_velocity(x, y, z);
+
+                        double u_exact, v_exact, w_exact, p_exact;
+
+                        omega.exact_solution(coordinate, nu, t, out u_exact, out v_exact,
+                                out w_exact, out p_exact);
+
+                        switch (component)
+                        {
+                            case 1:
+                                comp_interp[i, j, k] = de.get_pressure(x, y, z);
+                                err_array[i, j, k] = Math.Abs(de.get_pressure(x, y, z) - p_exact);
+                                 break;
+
+                            case 2:
+                                comp_interp[i, j, k] = velocity_interp[0];
+                                err_array[i, j, k] = Math.Abs(velocity_interp[0] - u_exact);
+                                break;
+
+                            case 3:
+                                comp_interp[i, j, k] = velocity_interp[1];
+                                err_array[i, j, k] = Math.Abs(velocity_interp[1] - v_exact);
+                                break;
+
+                            case 4:
+                                comp_interp[i, j, k] = velocity_interp[2];
+                                err_array[i, j, k] = Math.Abs(velocity_interp[2] - w_exact);
+                                break;
+
+                        }
+                    }
+                }
+            }
+
+            err_l2 = Utilities.compute_L2_difference(err_array, zeros); //L2 norm of errors
+            err_inf = err_array.Cast<double>().Max();
+}
     }
 }
