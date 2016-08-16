ead4f06826fc0646f6b7a1415ff17d56ca19c174
Lukas <Lukas>
2016-08-16 16:02:08 -0400

    Added comments, cleaned up code

--- Test/PostProcessor.cs
+++ Test/PostProcessor.cs
@@ -7,15 +7,25 @@ using System.IO;
 
 namespace FastFluidSolver
 {
+    /// <summary>
+    /// Class for PostProcessing data calculated by the FFD solver in FluidSolver.cs.
+    /// Exports data to VTK files for further analysis. VTK files
+    /// are used often for visualization of scientific data. These files can be read
+    /// by Visit, Paraview and other applications. A toolkit also exists in C++ for analyzing
+    /// this data. More info on VTK is avaiable here: http://www.vtk.org.
+    /// </summary>
     public class PostProcessor
     {
         FluidSolver fs;
         Domain omega;
         DataExtractor de;
 
-        /***************************************************************
-         * Constructor
-         **************************************************************/
+       /// <summary>
+       /// Constructor
+       /// </summary>
+       /// <param name="fs">FluidSolver containing solution</param>
+       /// <param name="omega">Domain containing geometry information and 
+       /// exact solutions (if known)</param>
         public PostProcessor(FluidSolver fs, Domain omega)
         {
             this.fs = fs;
@@ -24,15 +34,16 @@ namespace FastFluidSolver
             de = new DataExtractor(omega, fs);
         }
 
-        /*****************************************************************************
-        * Export data to a VTK file for visualization, based on file format guide here:
-        * http://www.vtk.org/wp-content/uploads/2015/04/file-formats.pdf
-        * 
-        * Nx, Ny, Nz are the number of CELLS (not points) in each direction.
-        ****************************************************************************/
+        /// <summary>
+        /// Exports data to a VTK file. Data can be cell centred or vertex centred. 
+        /// </summary>
+        /// <param name="fname">file name</param>
+        /// <param name="time">time</param>
+        /// <param name="cell_centred">cell centred flag</param>
+        /// <remarks>The file format guide for VTK is avaliable here:
+        /// http://www.vtk.org/wp-content/uploads/2015/04/file-formats.pdf </remarks>
         public void export_data_vtk(String fname, double time, bool cell_centred)
         {
-
             int Nx, Ny, Nz;
 
             double hx = omega.hx;
@@ -45,6 +56,13 @@ namespace FastFluidSolver
             double[, ,] p_interp;
             double[, ,] div_interp;
 
+            double[, ,] err_u;
+            double[, ,] err_v;
+            double[, ,] err_w;
+            double[, ,] err_p;
+            
+            interpolate_errors(out err_u, out err_v, out err_w, out err_p, time);
+
             if (cell_centred)
             {
                 Nx = omega.Nx - 2;
@@ -54,12 +72,14 @@ namespace FastFluidSolver
                 interpolate_to_cell_centre_grid(out u_interp, out v_interp, out w_interp, out p_interp, out div_interp);
             }
             else
-            {
+            {               
+
                 Nx = omega.Nx - 1;
                 Ny = omega.Ny - 1;
                 Nz = omega.Nz - 1;
-
-                interpolate_to_vertex_grid(out u_interp, out v_interp, out w_interp, out p_interp, out div_interp);
+                
+                interpolate_to_vertex_grid(out u_interp, out v_interp, out w_interp, out p_interp);
+                div_interp = new double[1, 1, 1]; //have to assign div_interp to avoid compiling errors
             }
 
             using (StreamWriter sw = new StreamWriter(fname))
@@ -138,12 +158,70 @@ namespace FastFluidSolver
                         }
                     }
                 }
+                else
+                {
+                    sw.WriteLine("SCALARS err_u double {0}", 1);
+                    sw.WriteLine("LOOKUP_TABLE default");
+                    for (int k = 0; k < Nz; k++)
+                    {
+                        for (int j = 0; j < Ny; j++)
+                        {
+                            for (int i = 0; i < Nx; i++)
+                            {
+                                sw.Write("{0} ", err_u[i, j, k]);
+                            }
+                        }
+                    }
+
+                    sw.WriteLine("SCALARS err_v double {0}", 1);
+                    sw.WriteLine("LOOKUP_TABLE default");
+                    for (int k = 0; k < Nz; k++)
+                    {
+                        for (int j = 0; j < Ny; j++)
+                        {
+                            for (int i = 0; i < Nx; i++)
+                            {
+                                sw.Write("{0} ", err_v[i, j, k]);
+                            }
+                        }
+                    }
+
+                    sw.WriteLine("SCALARS err_w double {0}", 1);
+                    sw.WriteLine("LOOKUP_TABLE default");
+                    for (int k = 0; k < Nz; k++)
+                    {
+                        for (int j = 0; j < Ny; j++)
+                        {
+                            for (int i = 0; i < Nx; i++)
+                            {
+                                sw.Write("{0} ", err_w[i, j, k]);
+                            }
+                        }
+                    }
+
+                    sw.WriteLine("SCALARS err_p double {0}", 1);
+                    sw.WriteLine("LOOKUP_TABLE default");
+                    for (int k = 0; k < Nz; k++)
+                    {
+                        for (int j = 0; j < Ny; j++)
+                        {
+                            for (int i = 0; i < Nx; i++)
+                            {
+                                sw.Write("{0} ", err_p[i, j, k]);
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        /*****************************************************************************
-            * Export geometry to a VTK file for visualization
-        *****************************************************************************/
+       
+        /// <summary>
+        /// Exports geometry, including obstacle cells (excluding ghost cells), boundary cells
+        /// and outflow cells to a VTK file
+        /// </summary>
+        /// <param name="fname">file name</param>
+        /// <param name="time">time</param>
        public void export_geometry_vtk(String fname, double time)
        {
            double hx = omega.length_x / (omega.Nx - 2);
@@ -194,11 +272,20 @@ namespace FastFluidSolver
                sw.WriteLine("LOOKUP_TABLE default");
                for (int k = 0; k < omega.Nz; k++)
                {
-                   for (int j = 0; j < omega.Ny; j++)
+                   for (int j = 0; j < omega.Ny ; j++)
                    {
-                       for (int i = 0; i < omega.Nx; i++)
+                       for (int i = 0; i < omega.Nx ; i++)
                        {
-                           sw.Write("{0} ", (double) omega.obstacle_cells[i, j, k]);
+                           if (i > 0 && i < omega.Nx - 1
+                               && j > 0 && j < omega.Ny - 1
+                               && k > 0 && k < omega.Nz - 1)
+                           {
+                               sw.Write("{0} ", (double)omega.obstacle_cells[i, j, k]);
+                           }
+                           else
+                           {
+                               sw.Write("{0} ", 0);
+                               }
                        }
                    }
                }
@@ -242,7 +329,7 @@ namespace FastFluidSolver
                    }
                }
 
-               sw.WriteLine("SCALARS outflow_cells double {0}", 1);
+               sw.WriteLine("SCALARS outflow_boundary_x double {0}", 1);
                sw.WriteLine("LOOKUP_TABLE default");
                for (int k = 0; k < omega.Nz; k++)
                {
@@ -250,18 +337,107 @@ namespace FastFluidSolver
                    {
                        for (int i = 0; i < omega.Nx; i++)
                        {
-                           sw.Write("{0} ", (double) omega.outflow_cells[i, j, k]);
+                           sw.Write("{0} ", (double) omega.outflow_boundary_x[i, j, k]);
+                       }
+                   }
+               }
+
+               sw.WriteLine("SCALARS outflow_boundary_y double {0}", 1);
+               sw.WriteLine("LOOKUP_TABLE default");
+               for (int k = 0; k < omega.Nz; k++)
+               {
+                   for (int j = 0; j < omega.Ny; j++)
+                   {
+                       for (int i = 0; i < omega.Nx; i++)
+                       {
+                           sw.Write("{0} ", (double)omega.outflow_boundary_y[i, j, k]);
+                       }
+                   }
+               }
+
+               sw.WriteLine("SCALARS outflow_boundary_z double {0}", 1);
+               sw.WriteLine("LOOKUP_TABLE default");
+               for (int k = 0; k < omega.Nz; k++)
+               {
+                   for (int j = 0; j < omega.Ny; j++)
+                   {
+                       for (int i = 0; i < omega.Nx; i++)
+                       {
+                           sw.Write("{0} ", (double)omega.outflow_boundary_z[i, j, k]);
                        }
                    }
                }
            }
        }
 
-       /************************************************************************************
-        * Interpolates pressures and velocities from fs to a cell centred grid
-        * 
-        * TO DO: implement in parallel 
-        ***********************************************************************************/
+        
+      /// <summary>
+      /// Exports an uninterpolated pressure field to a VTK file
+      /// </summary>
+      /// <param name="fname">file name</param>
+      /// <param name="time">time</param>
+       public void export_uninterpolated_vtk(String fname, double time)
+       {
+           int Nx = fs.p.GetLength(0);
+           int Ny = fs.p.GetLength(1);
+           int Nz = fs.p.GetLength(2);
+
+           double hx = omega.hx;
+           double hy = omega.hy;
+           double hz = omega.hz;
+
+           using (StreamWriter sw = new StreamWriter(fname))
+           {
+               sw.WriteLine("# vtk DataFile Version 3.0");
+               sw.WriteLine("Fast Fluid Dynamics data\n");
+               sw.WriteLine("ASCII");
+               sw.WriteLine("DATASET RECTILINEAR_GRID");
+               sw.WriteLine("FIELD FieldData 1");
+               sw.WriteLine("TIME 1 1 double");
+               sw.WriteLine("{0}", time);
+               sw.WriteLine("DIMENSIONS {0} {1} {2}", omega.Nx + 1, omega.Ny + 1, omega.Nz + 1);
+               sw.WriteLine("X_COORDINATES {0} double", omega.Nx + 1);
+
+               for (int i = 0; i < omega.Nx + 1; i++)
+               {
+                   sw.WriteLine("{0}", (i - 1) * hx);
+               }
+               sw.WriteLine("Y_COORDINATES {0} double", omega.Ny + 1);
+               for (int i = 0; i < omega.Ny + 1; i++)
+               {
+                   sw.WriteLine("{0}", (i - 1) * hy);
+               }
+               sw.WriteLine("Z_COORDINATES {0} double", omega.Nz + 1);
+               for (int i = 0; i < omega.Nz + 1; i++)
+               {
+                   sw.WriteLine("{0}", (i - 1) * hz);
+               }
+
+               sw.WriteLine("CELL_DATA {0}", Nx * Ny * Nz);
+               sw.WriteLine("SCALARS p_uninterp double {0}", 1);
+               sw.WriteLine("LOOKUP_TABLE default");
+               for (int k = 0; k < Nz; k++)
+               {
+                   for (int j = 0; j < Ny; j++)
+                   {
+                       for (int i = 0; i < Nx; i++)
+                       {
+                           sw.Write("{0} ", fs.p[i, j, k]);
+                       }
+                   }
+               }
+           }
+       }
+
+      
+        /// <summary>
+        /// Interpolated velocity components, pressure and divergence field to cell centred grid.
+        /// </summary>
+        /// <param name="u_interp">interpolated x component of velocity</param>
+        /// <param name="v_interp">interpolated y component of velocity</param>
+        /// <param name="w_interp">interpolated z component of velocity</param>
+        /// <param name="p_interp">interpolated pressure</param>
+        /// <param name="div_interp">interpolated divergence</param>
         private void interpolate_to_cell_centre_grid(out double[, ,] u_interp, out double[, ,] v_interp, 
                     out double[, ,] w_interp, out double[, ,] p_interp, out double[, ,] div_interp)
         {
@@ -281,9 +457,9 @@ namespace FastFluidSolver
                 {
                     for (int k = 1; k < Nz + 1; k++)
                     {
-                        if (omega.obstacle_cells[i,j,k] == 0)
+                        //if (omega.obstacle_cells[i, j, k] == 0)
                         {
-                            double[] velocity_interp = de.get_velocity((i - 0.5) * omega.hx, (j - 0.5) * omega.hy, (k - 0.5) * omega.hz); 
+                            double[] velocity_interp = de.get_velocity((i - 0.5) * omega.hx, (j - 0.5) * omega.hy, (k - 0.5) * omega.hz);
                             u_interp[i - 1, j - 1, k - 1] = velocity_interp[0];
                             v_interp[i - 1, j - 1, k - 1] = velocity_interp[1];
                             w_interp[i - 1, j - 1, k - 1] = velocity_interp[2];
@@ -292,19 +468,22 @@ namespace FastFluidSolver
 
                             div_interp[i - 1, j - 1, k - 1] = (fs.u[i, j, k] - fs.u[i - 1, j, k]) / omega.hx + (fs.v[i, j, k] - fs.v[i, j - 1, k]) / omega.hy +
                                     (fs.w[i, j, k] - fs.w[i, j, k - 1]) / omega.hz;
-                         }
+                        }
                     }
                 }
             }
         }
 
-        /************************************************************************************
-         * Interpolates pressures and velocities from fs to a vertex centred grid
-         * 
-         * TO DO: implement in parallel 
-         ***********************************************************************************/
+      
+        /// <summary>
+        /// Interpolates velocity components and pressure to a vertex centred grid.
+        /// </summary>
+        /// <param name="u_interp">interpolated x component of velocity</param>
+        /// <param name="v_interp">interpolated y component of velocity</param>
+        /// <param name="w_interp">interpolated z component of velocity</param>
+        /// <param name="p_interp">intepolated pressure</param>
         private void interpolate_to_vertex_grid(out double[, ,] u_interp, out double[, ,] v_interp,
-                    out double[, ,] w_interp, out double[, ,] p_interp, out double[, ,] div_interp)
+                    out double[, ,] w_interp, out double[, ,] p_interp)
         {
             int Nx = omega.Nx - 1;
             int Ny = omega.Ny - 1;
@@ -314,7 +493,6 @@ namespace FastFluidSolver
             v_interp = new double[Nx, Ny, Nz];
             w_interp = new double[Nx, Ny, Nz];
             p_interp = new double[Nx, Ny, Nz];
-            div_interp = new double[Nx, Ny, Nz];
 
             for (int i = 0; i < Nx; i++)
             {
@@ -328,6 +506,58 @@ namespace FastFluidSolver
                         w_interp[i, j, k] = velocity_interp[2];
 
                         p_interp[i, j, k] = de.get_pressure(i * omega.hx, j * omega.hy, k* omega.hz);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Interpolates the errors in u, v, w and p on a Domain on which an exact solution is known.
+        /// </summary>
+        /// <param name="err_u">error in x component of velocity</param>
+        /// <param name="err_v">error in ycomponent of velocity</param>
+        /// <param name="err_w">error in z component of velocity</param>
+        /// <param name="err_p">error in pressure</param>
+        /// <param name="t">time</param>
+        private void interpolate_errors(out double[, ,] err_u, out double[, ,] err_v, 
+            out double[, ,] err_w, out double [, ,] err_p, double t)
+        {
+            int Nx = omega.Nx - 1;
+            int Ny = omega.Ny - 1;
+            int Nz = omega.Nz - 1;
+
+
+            err_u = new double[Nx, Ny, Nz];
+            err_v = new double[Nx, Ny, Nz];
+            err_w = new double[Nx, Ny, Nz];
+            err_p = new double[Nx, Ny, Nz];
+
+            for (int i = 0; i < Nx; i++)
+            {
+                for (int j = 0; j < Ny; j++)
+                {
+                    for (int k = 0; k < Nz; k++)
+                    {
+                        double x = i * omega.hx;
+                        double y = j * omega.hy;
+                        double z = k * omega.hz;
+
+                        double[] coordinate = new double[] { x, y, z };
+
+                        double[] velocity_interp = de.get_velocity(x, y, z);
+
+                        double u_exact, v_exact, w_exact, p_exact;
+
+                        omega.exact_solution(coordinate, fs.nu, t, out u_exact, out v_exact,
+                                out w_exact, out p_exact);
+
+                        err_u[i, j, k] = velocity_interp[0] - u_exact;
+
+                        err_v[i, j, k] = velocity_interp[1] - v_exact;
+
+                        err_w[i, j, k] = velocity_interp[2] - w_exact;
+
+                        err_p[i, j, k] = de.get_pressure(x, y, z) - p_exact;
                     }
                 }
             }
